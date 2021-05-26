88605873666ca8cc1fd3b2938ee239c15a56fec2
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-05-26 15:39:58 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -3,7 +3,11 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <cinttypes>
+
+#include <stdio.h>
+#include <inttypes.h>
+
+#include <cinttypes.h>
 
 
 #include <chainparams.h>
