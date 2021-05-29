1767bef9dcd5a47d2cd9b87d549defbd1b9e6c0a
dhweinberg <dhweinberg80@gmail.com>
2021-05-29 18:39:56 -0400

    changes to chainparams.cpp for checkpoints

--- qa/pull-tester/tests_config.py
+++ qa/pull-tester/tests_config.py
@@ -3,8 +3,8 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-SRCDIR="/home/c4pt/Desktop/CURRENT-FORK/radioCOIN/radiocoin"
-BUILDDIR="/home/c4pt/Desktop/CURRENT-FORK/radioCOIN/radiocoin"
+SRCDIR="/home/c4pt/Desktop/FORK-05-29-2021/radioCOIN"
+BUILDDIR="/home/c4pt/Desktop/FORK-05-29-2021/radioCOIN"
 EXEEXT=""
 
 # These will turn into comments if they were disabled when configuring.
