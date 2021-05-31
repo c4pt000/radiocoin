60a2b1a91130399c64da4e07857d930a8fcb7aa6
dhweinberg <dhweinberg80@gmail.com>
2021-05-31 17:01:10 -0400

    some slight changes

--- qa/pull-tester/tests_config.py
+++ qa/pull-tester/tests_config.py
@@ -3,8 +3,8 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-SRCDIR="/home/c4pt/Desktop/UNCONFIRMEDBLOCK-radiocoin/radioCOIN/radioCOIN"
-BUILDDIR="/home/c4pt/Desktop/UNCONFIRMEDBLOCK-radiocoin/radioCOIN/radioCOIN"
+SRCDIR="/home/c4pt/Desktop/CURRENT-FORK/radiocoin"
+BUILDDIR="/home/c4pt/Desktop/CURRENT-FORK/radiocoin"
 EXEEXT=""
 
 # These will turn into comments if they were disabled when configuring.
