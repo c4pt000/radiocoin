bd815d5840b701ff909a82a8619a754acbd01b10
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-06-13 20:11:32 -0400

    Update build-for-windows-exe.sh

--- build-for-windows-exe.sh
+++ build-for-windows-exe.sh
@@ -16,6 +16,38 @@ make HOST=x86_64-w64-mingw32
 cd ..
 ./autogen.sh 				# not required when building from tarball
 CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --with-incompatible-bdb --prefix=/usr --enable-sse2
+
+
+echo "DONT FORGET TO CHANGE COMPILER BOTH MINGW-GCC AND MINGW-G++ TO POSIX SINCE WINDOWS DOESNT REALLY SUPPORT MUTEX WITH SPINLOCKS"
+echo "
+update-alternatives --config x86_64-w64-mingw32-g++
+There are 2 choices for the alternative x86_64-w64-mingw32-g++ (providing /usr/bin/x86_64-w64-mingw32-g++).
+
+  Selection    Path                                   Priority   Status
+------------------------------------------------------------
+  0            /usr/bin/x86_64-w64-mingw32-g++-win32   60        auto mode
+* 1            /usr/bin/x86_64-w64-mingw32-g++-posix   30        manual mode
+  2            /usr/bin/x86_64-w64-mingw32-g++-win32   60        manual mode
+
+Press <enter> to keep the current choice[*], or type selection number: 
+
+
+
+update-alternatives --config x86_64-w64-mingw32-gcc
+There are 2 choices for the alternative x86_64-w64-mingw32-gcc (providing /usr/bin/x86_64-w64-mingw32-gcc).
+
+  Selection    Path                                   Priority   Status
+------------------------------------------------------------
+  0            /usr/bin/x86_64-w64-mingw32-gcc-win32   60        auto mode
+* 1            /usr/bin/x86_64-w64-mingw32-gcc-posix   30        manual mode
+  2            /usr/bin/x86_64-w64-mingw32-gcc-win32   60        manual mode
+
+Press <enter> to keep the current choice[*], or type selection number: 
+
+
+"
+<std::mutex>
+
 make -j200    				#(where 200 is cpu core count)
 sleep 2s
 echo "whats a cyberpunk thriller without money for radios anyway?"
