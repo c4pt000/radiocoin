ebeb882af645220fa8202bf0874f6b26ae78f91a
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-06-19 10:34:40 -0400

    Update build-for-windows-exe.sh

--- build-for-windows-exe.sh
+++ build-for-windows-exe.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 echo "requires mingw32 ubuntu:20.10 as a build environment, the script will pause for 10 seconds, i'll dream in bogomips how great virtualized environments can be,"
-sleep 10s
+echo "
+apt-get update -y 
+apt-get install build-essential *mingw* aclocal automake libtool curl libdb* protobuf* \
+libboost* libssl-dev qt5-* -y"
+sleep 3s
 #mkdir /opt/winradio
 
 #Then build using: (windows x86_64 64bit .exe binaries)
-
-
-apt-get update -y 
-apt-get install build-essential *mingw* aclocal automake libtool curl libdb* protobuf* \
-libboost* libssl-dev qt5-* -y
-
-
-cd depends
 echo "
+win32 build-env for mingw32
+
+docker run -it -d -v /opt/radiocoin:/opt/radiocoin c4pt/radiocoinwinbuild2010
+docker exec -it <docker_vm> bash
+sleep 2s
+"
+
+
+echo "
+cd depends
 must be in win32 mode for depends build change to posix for actual build
 update-alternatives --config x86_64-w64-mingw32-gcc
 There are 2 choices for the alternative x86_64-w64-mingw32-gcc (providing /usr/bin/x86_64-w64-mingw32-gcc).
@@ -31,7 +37,7 @@ There are 2 choices for the alternative x86_64-w64-mingw32-gcc (providing /usr/b
 * 0            /usr/bin/x86_64-w64-mingw32-gcc-win32   60        auto mode
   1            /usr/bin/x86_64-w64-mingw32-gcc-posix   30        manual mode
   2            /usr/bin/x86_64-w64-mingw32-gcc-win32   60        manual mode
-"
+
 
 
 make -j200 HOST=x86_64-w64-mingw32
@@ -40,8 +46,8 @@ cd ..
 CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --with-incompatible-bdb --prefix=/usr --enable-sse2
 
 
-echo "DONT FORGET TO CHANGE COMPILER BOTH MINGW-GCC AND MINGW-G++ TO POSIX SINCE WINDOWS DOESNT REALLY SUPPORT MUTEX WITH SPINLOCKS"
-echo "
+echo DONT FORGET TO CHANGE COMPILER BOTH MINGW-GCC AND MINGW-G++ TO POSIX SINCE WINDOWS DOESNT REALLY SUPPORT MUTEX WITH SPINLOCKS
+
 update-alternatives --config x86_64-w64-mingw32-g++
 There are 2 choices for the alternative x86_64-w64-mingw32-g++ (providing /usr/bin/x86_64-w64-mingw32-g++).
 
@@ -67,9 +73,10 @@ There are 2 choices for the alternative x86_64-w64-mingw32-gcc (providing /usr/b
 Press <enter> to keep the current choice[*], or type selection number: 
 
 
-"
+
 
 make -j200    				#(where 200 is cpu core count)
+"
 sleep 2s
 echo "whats a cyberpunk thriller without money for radios anyway?"
 
