ad15a973c723b672c52deb85630941bd7146800f
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-06-20 21:05:11 -0400

    Update cgminer.bat

--- cgminer.bat
+++ cgminer.bat
@@ -1,2 +1,3 @@
-C:\Users\YOURUSER\Desktop\cgminer-3.7.2-windows\cgminer.exe -I 13 --scrypt -o stratum+tcp://radiopool.me:9555 --userpass YOUR-RADIOCOIN_RECEIVE_ADDR_HERE:x --worksize 64 -g 1 --lookup-gap 2 --thread-concurrency 7200 --fix-protocol
+C:\Users\YOURUSER\Desktop\cgminer-3.7.2-windows\cgminer.exe -I 16 --scrypt -o stratum+tcp://radiopool.me:9555 --userpass YOUR-RADIOCOIN_RECEIVE_ADDR_HERE:x --worksize 128 -g 1 --lookup-gap 2 --thread-concurrency 7200 --fix-protocol
 
+ 
