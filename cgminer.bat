40b0b2b9ecfdd5f9e77304210b193a786ddfabf5
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-06-20 21:24:28 -0400

    Update cgminer.bat

--- cgminer.bat
+++ cgminer.bat
@@ -1,3 +1,8 @@
-C:\Users\YOURUSER\Desktop\cgminer-3.7.2-windows\cgminer.exe -I 13 --scrypt -o stratum+tcp://radiopool.me:9555 --userpass YOUR-RADIOCOIN_RECEIVE_ADDR_HERE:x --worksize 128 -g 1 --lookup-gap 2 --thread-concurrency 7200 --fix-protocol
+#slow card -I 11
+
+C:\Users\YOURUSER\Desktop\cgminer-3.7.2-windows\cgminer.exe -I 11 --scrypt -o stratum+tcp://radiopool.me:9555 --userpass YOUR-RADIOCOIN_RECEIVE_ADDR_HERE:x --worksize 128 -g 1 --lookup-gap 2 --thread-concurrency 7200 --fix-protocol
 
  
+# RTX6000 / AMD wx7200 etc          be warned higher intensity card can break or cgminer might auto shutoff due to thermal temps
+
+C:\Users\YOURUSER\Desktop\cgminer-3.7.2-windows\cgminer.exe -I 20 --scrypt -o stratum+tcp://radiopool.me:9555 --userpass YOUR-RADIOCOIN_RECEIVE_ADDR_HERE:x --worksize 128 -g 1 --lookup-gap 2 --thread-concurrency 7200 --fix-protocol
