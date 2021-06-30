39a75b9c8e494fca252a9709419a4e0cfaa19f92
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-06-30 10:43:44 -0400

    Update amount.h

--- src/amount.h
+++ src/amount.h
@@ -28,7 +28,7 @@ extern const std::string CURRENCY_UNIT;
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */ //100B
-static const CAmount MAX_MONEY = 100000000000 * COIN; // RadioCoin: maximum of 1T coins (given some randomness), max transaction 10,000,000,000
+static const CAmount MAX_MONEY = 10000000000 * COIN; // RadioCoin: maximum of 1T coins (given some randomness), max transaction 10,000,000,000
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /**
