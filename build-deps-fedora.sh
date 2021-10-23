caaadc4ac7cb426b40710dd8a0cbd2544e8a9a75
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-10-22 21:40:05 -0400

    Update build-deps-fedora.sh

--- build-deps-fedora.sh
+++ build-deps-fedora.sh
@@ -1,3 +1,9 @@
+yum update -y
+yum groupinstall "C Development Tools and Libraries" -y
+
+# skip for just the qt desktop binary
+yum install --exclude=*nvidia* mesa* xorg* caja -y
+
 yum install git-core libdb-cxx-devel libdb-cxx openssl-devel 
 libevent-devel cppzmq-devel qrencode-devel qt5-qtbase-devel.x86_64 
 protobuf-devel boost-* boost-devel miniupnpc-devel.x86_64 
