ebdcc890445eef351c71abdb7dc991f1333b2e1a
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-10-22 21:38:09 -0400

    Create build-deps-fedora.sh

---
+++ build-deps-fedora.sh
@@ -0,0 +1,4 @@
+yum install git-core libdb-cxx-devel libdb-cxx openssl-devel 
+libevent-devel cppzmq-devel qrencode-devel qt5-qtbase-devel.x86_64 
+protobuf-devel boost-* boost-devel miniupnpc-devel.x86_64 
+diffutils qt-devel qt4-devel wget qt5-lin* alien dpkg -y
