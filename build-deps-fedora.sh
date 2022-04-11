9194d3cf96ad18d3669d3a8303f134d6aed4380e
19YBeABhyk9tY5z5zGbA3oQLU5MFExpb8o <46433702+c4pt000@users.noreply.github.com>
2022-04-10 20:24:49 -0700

    Update build-deps-fedora.sh

--- build-deps-fedora.sh
+++ build-deps-fedora.sh
@@ -7,6 +7,6 @@ yum groupinstall "C Development Tools and Libraries" -y
 yum install --exclude=*nvidia* mesa* xorg* caja -y
 
 yum install git-core libdb-cxx-devel libdb-cxx openssl-devel \
-libevent-devel cppzmq-devel qrencode-devel qt5-qtbase-devel.x86_64 \
-protobuf-devel boost-* boost-devel miniupnpc-devel.x86_64 \
+libevent-devel cppzmq-devel qrencode-devel qt5-qtbase-devel \
+protobuf-devel boost-* boost-devel miniupnpc-devel \
 diffutils qt-devel qt4-devel wget qt5-lin* alien dpkg qt5-qtbase -y
